e93315d9d0f8ad603be27229d382d7180cac8d8d
Pranayagaram <50867481+Pranayagaram@users.noreply.github.com>
2021-02-05 16:14:45 +0530

    Update validator_charlie.sh
    Added the port "--port 30333"

--- scripts/validator_charlie.sh
+++ scripts/validator_charlie.sh
@@ -1,2 +1,2 @@
 rm -r -f /tmp/bob/
-../target/release/node-polkadex --base-path /tmp/bob --chain customSpecRaw.json --charlie --validator --ws-port 9955 --ws-external --rpc-methods=unsafe --node-key 0000000000000000000000000000000000000000000000000000000000000003 --execution Native
+../target/release/node-polkadex --base-path /tmp/bob --chain customSpecRaw.json --charlie --validator --port 30333 --ws-port 9955 --ws-external --rpc-methods=unsafe --node-key 0000000000000000000000000000000000000000000000000000000000000003 --execution Native
