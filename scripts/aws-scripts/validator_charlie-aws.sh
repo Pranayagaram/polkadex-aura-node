728dd4bdb99b76df9fa68b2238dbde3c99163af3
Pranayagaram <50867481+Pranayagaram@users.noreply.github.com>
2021-02-17 12:49:26 +0530

    Update validator_charlie-aws.sh

--- scripts/aws-scripts/validator_charlie-aws.sh
+++ scripts/aws-scripts/validator_charlie-aws.sh
@@ -1,3 +1,3 @@
 rm -r -f /tmp/bob/
 /polkadex-aura-node/prometheus/prometheus --config.file /polkadex-aura-node/prometheus/prometheus.yml &
-/polkadex-aura-node/target/release/node-polkadex --base-path /tmp/bob --chain /polkadex-aura-node/scripts/customSpecRaw.json --charlie --validator --ws-port 9955 --ws-external --rpc-methods=unsafe --node-key 0000000000000000000000000000000000000000000000000000000000000003 --execution Native --prometheus-external
+/polkadex-aura-node/target/release/node-polkadex --base-path /tmp/bob --chain /polkadex-aura-node/scripts/customSpecRaw.json --charlie --validator --ws-port 9955 --port 30333 --node-key 0000000000000000000000000000000000000000000000000000000000000003 --execution Native --prometheus-external
