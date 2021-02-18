296e30ea6ed1c8aeee8e1f5ee8e6ed3b2cca6f8a
Pranayagaram <50867481+Pranayagaram@users.noreply.github.com>
2021-02-18 12:42:30 +0530

    added upserver scriptsin script for AWS CICD-2.6

--- scripts/aws-scripts/createCustomSpec.sh
+++ scripts/aws-scripts/createCustomSpec.sh
@@ -1,2 +1,3 @@
+#!/bin/ash
 /polkadex-aura-node/target/release/node-polkadex build-spec  --chain local > /polkadex-aura-node/scripts/customSpec.json
 /polkadex-aura-node/target/release/node-polkadex build-spec --chain=customSpec.json --raw > /polkadex-aura-node/scripts/customSpecRaw.json
