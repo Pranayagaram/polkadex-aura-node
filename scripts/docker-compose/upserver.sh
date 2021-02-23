f26a16a60149f636cf44cafeeb32a5387448bfd4
Pranayagaram <50867481+Pranayagaram@users.noreply.github.com>
2021-02-23 13:07:19 +0530

    added docker docker-compose scripts 1.0

---
+++ scripts/docker-compose/upserver.sh
@@ -0,0 +1,2 @@
+#/polkadex-aura-node/prometheus/prometheus --config.file /polkadex-aura-node/prometheus/prometheus.yml &
+/polkadex-aura-node/target/release/node-polkadex --chain /polkadex-aura-node/scripts/customSpecRaw.json --dave --port 30333 --ws-port 9955 --rpc-port 9956 --ws-external --node-key 0000000000000000000000000000000000000000000000000000000000000005 --bootnodes /dns/bootnode_alice/tcp/30333/p2p/12D3KooWEyoppNCUx8Yx66oV9fJnriXwCcXwDDUA2kj6vnc6iDEp --prometheus-external
